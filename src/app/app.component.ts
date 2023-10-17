4b6255d510e1e26b83b1f9d69c60c13c4ba2f0e3
Deep <105926444+Dpppatel@users.noreply.github.com>
2023-10-17 14:01:25 -0500

    Both are same

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,16 @@
 // import {HttpClient} from '@angular/common/http';
 
 // @Component({
-  // selector: 'app-root',
-  // templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
+// selector: 'app-root',
+// templateUrl: './app.component.html',
+// styleUrls: ['./app.component.css']
 // })
 // export class AppComponent {
-  // title = 'User';
-  // data = {}  as any;
-  //constructor(private http: HttpClient) {
-    // http.get('resource').subscribe(data => this.data = data);
-  // }
+// title = 'User';
+// data = {}  as any;
+//constructor(private http: HttpClient) {
+// http.get('resource').subscribe(data => this.data = data);
+// }
 // }
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -19,12 +19,16 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Demo';
-  data = {}  as any;
-  constructor(private http: HttpClient) {
-    http.get('resource').subscribe(data => this.data = data);
+  chartType: string = 'chartJs';
+
+  changeChart() {
+    if (this.chartType == 'chartJs') {
+      this.chartType = 'Angular Charts';
+      return;
+    }
+    this.chartType = 'chartJs';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { NgChartsModule } from 'ng2-charts';
+import { ChartJsComponent } from './chart-js/chart-js.component';
+import { AngularChartsComponent } from './angular-charts/angular-charts.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartJsComponent,
+    AngularChartsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
