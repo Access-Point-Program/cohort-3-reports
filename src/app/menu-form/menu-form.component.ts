62028ec8853695b83182bc7f81157680e17af1bc
epricewillis <epricewillis@gmail.com>
2023-11-07 11:16:37 -0600

    finished

--- src/app/menu-form/menu-form.component.ts
+++ src/app/menu-form/menu-form.component.ts
@@ -14,6 +14,7 @@ export class MenuFormComponent {
   ruleset = [' RuleSet Filter One', ' RuleSet Filter Two',
             'RuleSet Filter Three', 'RuleSet Filter Four'];
   rulesetselection?: string;
+  powersselection?: string;
 
   model = new Menu(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
 
@@ -21,6 +22,7 @@ export class MenuFormComponent {
   onSubmit() { this.submitted = true; }
 
    selection(one:string){this.rulesetselection= one} 
+   selections(one:string){this.powersselection= one}
    
 }
 
