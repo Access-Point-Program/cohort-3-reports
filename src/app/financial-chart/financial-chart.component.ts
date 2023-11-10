b685343e04a21c6df542852a40d676464c6c3f54
Deep <105926444+Dpppatel@users.noreply.github.com>
2023-11-09 23:48:28 -0600

    financial chart DONE!!!

--- src/app/financial-chart/financial-chart.component.ts
+++ src/app/financial-chart/financial-chart.component.ts
@@ -4,6 +4,8 @@ import {
   CandlestickController,
   CandlestickElement,
 } from 'chartjs-chart-financial';
+import 'chartjs-adapter-date-fns';
+import { enUS } from 'date-fns/locale';
 
 @Component({
   selector: 'app-financial-chart',
@@ -11,56 +13,7 @@ import {
   styleUrls: ['./financial-chart.component.css']
 })
 export class FinancialChartComponent {
-  barCount = 60;
-  initialDateStr = '2017-04-01T00:00:00';
-
-  public financialChartData: ChartConfiguration['data'] = {
-    datasets: [
-      {
-        label: 'Example Chart data',
-        data: [
-          {
-            "c": 1,
-            "x": 100,
-            "h": 110,
-            "l": 90,
-            "o": 105
-          },
-          {
-            "c": 2,
-            "x": 200,
-            "h": 210,
-            "l": 190,
-            "o": 205
-          },
-          {
-            "c": 3,
-            "x": 300,
-            "h": 310,
-            "l": 290,
-            "o": 305
-          }
-        ]
-         // Data goes here
-      },
-    ],
-  };
-
-  public financialChartOptions: ChartConfiguration['options'] = { // Configuration
-    responsive: true,
-    animation: false,
-    maintainAspectRatio: true,
-    
-    borderColor: 'black',
-    backgroundColor: 'black',
-    plugins: {
-      legend: {
-        display: true,
-      },
-    },
-  };
-
-  public financialChartType: ChartType = 'line';
+  financialChartType: ChartType = 'candlestick';
 
 
   constructor() {
@@ -70,34 +23,118 @@ export class FinancialChartComponent {
     );
   }
 
-  randomNumber(min: number, max: number): number {
-    return Math.random() * (max - min) + min;
-  }
+  public financialChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        label: 'Example Chart data',
+        data: [
+          {
+            "x": 1669352400000,// X-axis -> date converted into timestamp
+            "h": 90,// High -> Ending point for the stick (HIGHEST POINT)
+            "c": 85,// Close -> Ending point of the Bar (END OF THE BAR, Lower than High)
+            "o": 0,// Open -> Starting point for the Bar (STRT OF THE BAR, Lower than Close and High, Higher than Low)
+            "l": 0,// Low -> Starting point for the stick (LOWEST POINT)
+          },
+          {
+            "x": 1669438800000,
+            "h": 95,
+            "c": 65,// If (Close < Open), the bar turns red
+            "o": 0,
+            "l": 0,
+          },
+          {
+            "x": 1669525200000,
+            "h": 102,
+            "c": 92,
+            "o": 0,
+            "l": 0,
+          },
+          {
+            "x": 1669611600000,
+            "h": 98,
+            "c": 88,
+            "o": 0,
+            "l": 0,
+          },
+          {
+            "x": 1669698000000,
+            "h": 105,
+            "c": 95,
+            "o": 0,
+            "l": 0,
+          },
+          {
+            "x": 1669784400000,
+            "h": 101,
+            "c": 91,
+            "o": 0,
+            "l": 0,
+          },
+          {
+            "x": 1669870800000,
+            "h": 104,
+            "c": 94,
+            "o": 0,
+            "l": 0,
+          },
+        ]
+        // Data goes here type: { c: number; x: number; h: number; l: number; o: number }[] 
+      },
+    ],
+  };
 
-  randomBar(
-    date: Date,
-    lastClose: number
-  ): { c: number; x: number; h: number; l: number; o: number } {
-    const open = this.randomNumber(lastClose * 0.95, lastClose * 1.05);
-    const close = this.randomNumber(open * 0.95, open * 1.05);
-    const high = this.randomNumber(
-      Math.max(open, close),
-      Math.max(open, close) * 1.1
-    );
-    const low = this.randomNumber(
-      Math.min(open, close) * 0.9,
-      Math.min(open, close)
-    );
-    return {
-      x: +date,
-      o: open,
-      h: high,
-      l: low,
-      c: close,
-    };
-  }
-
-  // { c: number; x: number; h: number; l: number; o: number }[] 
-    
-  
+  public financialChartOptions: ChartConfiguration['options'] = { // Configuration
+    responsive: true,
+    animation: false,
+    transitions: {
+      show: {
+        animations: {
+          x: {
+            from: 0
+          },
+          y: {
+            from: 0
+          }
+        }
+      },
+      hide: {
+        animations: {
+          x: {
+            to: 0
+          },
+          y: {
+            to: 0
+          }
+        }
+      }
+    },
+    maintainAspectRatio: true,
+    scales: {
+      x: {
+        time: {
+          unit: 'day',
+        },
+        adapters: {
+          date: {
+            locale: enUS,
+          },
+        },
+        ticks: {
+          source: 'auto',
+        }
+      },
+      y: {
+        ticks: {
+          source: 'auto'
+        }
+      }
+    },
+    borderColor: 'black',
+    backgroundColor: 'black',
+    plugins: {
+      legend: {
+        display: true,
+      },
+    },
+  };
 }
