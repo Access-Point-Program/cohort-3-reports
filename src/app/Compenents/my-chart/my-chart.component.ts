f3b4b4fd89328b998ac188d0253033bd06623b77
epricewillis <epricewillis@gmail.com>
2023-11-16 20:05:51 -0600

    chart

---
+++ src/app/Compenents/my-chart/my-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import{Chart} from 'node_modules/chart.js'
+@Component({
+  selector: 'app-my-chart',
+  templateUrl: './my-chart.component.html',
+  styleUrls: ['./my-chart.component.css']
+})
+export class MyChartComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,24 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
-import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MenuFormComponent } from './menu-form/menu-form.component';
+import { NgChartsModule } from 'ng2-charts';
+import { MyChartComponent } from './Compenents/my-chart/my-chart.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    MenuFormComponent
+    MenuFormComponent,
+    MyChartComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule 
+    NgChartsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
