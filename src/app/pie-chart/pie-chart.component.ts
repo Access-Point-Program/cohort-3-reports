368c94e413ffb57af4bd3dec914ee60309c254a2
bioncoder <bgrr2013@gmail.com>
2023-11-28 16:00:27 -0600

    shifting to radar attempts

--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { Results } from '../Results';
+import { Layout } from '../layout';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.css']
 })
-export class PieChartComponent {
+export class PieChartComponent implements OnInit, OnChanges{
+
+  @Input() Simulations: Results[] = [];
+
+  ngOnChanges(_changes: SimpleChanges): void {
+    
+  }
+
+
+  ngOnInit(): void {
+    
+  }
+
+
   // Pie
   public pieChartOptions: ChartConfiguration['options'] = {
     responsive: false,
@@ -15,11 +30,50 @@ export class PieChartComponent {
     },
   };
   public pieChartData: ChartConfiguration["data"] = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ["median", "mean", "mode", "range", "success"],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Pass' },
+      { data: [65, 59, 80, 81, 56], label: 'Pass' },
     ]
   };
-  public pieChartType: ChartType = 'pie';
+  public pieChartType: ChartType = 'radar';
+
+  private getData(){
+    const output = []
+    //Get all success
+    const hat: Record<string, Results[]> = this.Simulations.reduce<Record<string, Results[]>>((sorted, result) => {
+      if (!result.successful) return sorted;
+      //Sort by layout
+      if (!sorted[result.layout]) sorted[result.layout] = [];
+      
+      sorted[result.layout].push(result);
+      
+      return sorted;
+    }, {});
+
+    //Success: Get length of each list per layout
+    for(const key in hat){
+        let success = hat[key].length;
+        let median = hat[key].sort()
+
+      for(const lock of hat[key]){
+        //Median: Find median of success by sorting the list and getting the midle value
+        
+
+
+
+      }
+
+    }
+
+
+    
+
+
+    //Mean: Add all numbers per layout and divide by their length
+
+    //Mode: Order array of success from least to greatest and get the number that appears the most
+
+    //Range: Find the difference between the highest and lowest value per layout
+  }
 
 }
