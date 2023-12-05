888de4c546bdc54ce9785bf1798e2c088ca878e2
Deep <105926444+Dpppatel@users.noreply.github.com>
2023-12-05 16:06:16 -0600

    made some progress with putting rules on the sheet. Still need to do the conditions.

--- src/app/exporter/exporter.component.ts
+++ src/app/exporter/exporter.component.ts
@@ -22,8 +22,7 @@ export class ExporterComponent implements OnInit {
   // api/rulesets-extended cyclones endpoint.
   
   
-  // WHEN Fact_type IS Value_type THEN Event_type
-  // WHEN (__ IS __) AND (__IS__) THEN __
+
 
 
   // API calls before this
@@ -46,15 +45,18 @@ export class ExporterComponent implements OnInit {
 
   private cellStyle(ws: XLSX.WorkSheet) {
 
-    const cn = [XLSX.utils.decode_col("A"), XLSX.utils.decode_col("B"), XLSX.utils.decode_col("C"),  XLSX.utils.decode_col("D"),  XLSX.utils.decode_col("E"),  XLSX.utils.decode_col("F")]
+    const cn = [XLSX.utils.decode_col("A"), XLSX.utils.decode_col("B"), XLSX.utils.decode_col("C"),  XLSX.utils.decode_col("D"),  XLSX.utils.decode_col("E"),  XLSX.utils.decode_col("F"), XLSX.utils.decode_col("G"), XLSX.utils.decode_col("H")];
 
     // Metadata
     ws['!cols'] = [];
 
-    for(const COL_INDEX of cn){
+    for(let i = 0; i < 8; i++){
       // create column metadata object if it does not exist
-      if(!ws["!cols"][COL_INDEX]) ws["!cols"][COL_INDEX] = {width: 14};
+      if(!ws["!cols"][i]) ws["!cols"][i] = {width: 14};
     }
+
+    const c = XLSX.utils.decode_row("1");
+    
 
     return ws;
   }
@@ -64,33 +66,38 @@ export class ExporterComponent implements OnInit {
     const tb = XLSX.utils.decode_range(`F1:H${ttl}`);
    
 
-
-    console.log({tb, ttl});
-
     ws[XLSX.utils.encode_cell({c: tb.s.c, r: 0})] = { v: 'Ruleset'};
     ws[XLSX.utils.encode_cell({c: tb.s.c+1, r: 0})] = { v: 'Rule'};
     ws[XLSX.utils.encode_cell({c: tb.s.c+2, r: 0})] = { v: 'Conditions'};
+
     // loop through row -> col
+    let lp = tb.s.r+1;
     for(const rule of this.constructR){
       const sumOfConditions = sum(rule.rules.reduce((acc:any, el:any) => el.conditions.length+acc, 0));
       const b = tb.s.c;
+        
+      //Ruleset
+      ws[XLSX.utils.encode_cell({c: b, r: lp})] = { v: rule.name };
+      
+      // Condition TODO
+      let str = 'WHEN ';
 
-      // BRAIN FRIEDDDDDD
-      for(let i = tb.s.r+1; i <= tb.e.r; i++){
-        
-        //Ruleset
-        ws[XLSX.utils.encode_cell({c: b, r: i})] = { v: rule.name };
-        
-        // Condition
-        ws[XLSX.utils.encode_cell({c: b+1, r: i})] = { v: rule.rules.length }
-        
-        // Conditions count
+      // WHEN Fact_type IS Value_type THEN Event_type
+      // WHEN (__ IS __) AND (__IS__) THEN __
+      rule.rules.map((el: any) => {
+        el.conditions.map((condition:any) => {
+          
+        })
+      });
 
-        ws[XLSX.utils.encode_cell({c: b+2, r: i})] = { v: sumOfConditions }
-      }
+      ws[XLSX.utils.encode_cell({c: b+1, r: lp})] = { v: rule.rules.length }
+      
+      // Conditions count
+      ws[XLSX.utils.encode_cell({c: b+2, r: lp})] = { v: sumOfConditions }
+      lp++;
     }
 
-    ws['!ref'] = `A1:H${this.constructR.length+1}`;
+    ws['!ref'] = `A1:H${Math.max(this.constructR.length+1, ttl+1)}`;
 
     return ws;
 
