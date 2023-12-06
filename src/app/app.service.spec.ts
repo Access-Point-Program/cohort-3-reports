a8c0592fea95687e864de20d8e80a2fb5ca89f52
epricewillis <epricewillis@gmail.com>
2023-12-06 11:13:16 -0600

    checked expected response

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -1,62 +1,74 @@
-import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { TestBed,  } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController, } from '@angular/common/http/testing';
 import { AppService } from './app.service';
-import { Results } from './Results';
-import { Ruleset } from './Ruleset';
-import { Layout } from './layout';
-import { of } from 'rxjs';
-
-
 
 describe('AppService', () => {
   let service: AppService;
-  let httpMock:HttpTestingController;
+  let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach( () => {
+  TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
         {
-          provide: AppService,
-          
-        }]
+          provide: AppService, HttpTestingController
+        }
+      ]
     });
+   
     service = TestBed.inject(AppService);
+    httpMock = TestBed.inject(HttpTestingController);
     
 });
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
   
-  it('http Results call', 
-  inject([HttpClient, HttpTestingController], (http: HttpClient, httpMock: HttpTestingController) => {
-    
-    http.get('/simulations').subscribe(service => {
-            expect(service).toEqual('Get');
-        });
-
-    }))
-    it('http Layout call', 
-    inject([HttpClient, HttpTestingController], (http: HttpClient, httpMock: HttpTestingController) => {
-      
-      http.get('/layouts').subscribe(service => {
-              expect(service).toEqual('Get');
-          });
   
-      }))
-      it('http Ruleset call', 
-      inject([HttpClient, HttpTestingController], (http: HttpClient, httpMock: HttpTestingController) => {
-        
-        http.get('/rulesets').subscribe(service => {
-                expect(service).toEqual('Get');
-            });
+   it ('should return results', () => {
+   service.getResults().subscribe(result =>{
+     expect(result).toBeTruthy();
     
-        }))
+     console.log('results passed');
+   });
+    
+   const call = httpMock.expectOne('/api/simulations');
+   expect(call.request.method).toEqual('GET');
+
+   call.flush(httpMock);
+   
+});
+
+it ('should return rulesets', () => {
+  service.getRulesets().subscribe(ruleset =>{
+    expect(ruleset).toBeTruthy();
+    console.log('rulesets passed');
+  });
+   
+  const calls = httpMock.expectOne('/api/rulesets');
+  expect(calls.request.method).toEqual('GET');
+
+  calls.flush(httpMock);
+  
+});
+
+it ('should return layouts', () => {
+  service.getLayouts().subscribe(layout=>{
+    expect(layout).toBeTruthy();
+    console.log('layouts passed');
+  });
+  
+  const called = httpMock.expectOne('/api/layouts');
+  expect(called.request.method).toEqual('GET');
+
+  called.flush(httpMock);
+  
+});
 
   //make call
   //intercept call
   //check expected response 
+//              
+});
 
 
-  });
